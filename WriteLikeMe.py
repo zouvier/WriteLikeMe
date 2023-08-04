5f0eb925208a73e6e6283070e4406a317a09bb5e
Zoumana Cisse <zoumanacisse692@gmail.com>
2023-08-04 14:49:34 -0700

    added more functionality

--- WriteLikeMe.py
+++ WriteLikeMe.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -47,21 +48,23 @@ if check_password():
 
     def get_input():
         input_type = st.selectbox("Select input type", ["Text","Link", "File"])
-        temperature = st.slider("Set the temperature:", min_value=0.0, max_value=1.0, step=0.1)
-        tone = st.checkbox("Tone")
-        voice = st.checkbox("Voice")
-        vocabulary = st.checkbox("Vocabulary")
-        sentence_structure = st.checkbox("Sentence structure")
+        temperature = st.slider("Creativity(0% - 100%):", value=0.5, min_value=0.0, max_value=1.0, step=0.1)
+        st.write("Copy the following from the Example text")
+        cols = st.columns(2)
+        tone = cols[0].checkbox("Tone")
+        voice = cols[0].checkbox("Voice")
+        vocabulary = cols[1].checkbox("Vocabulary")
+        sentence_structure = cols[1].checkbox("Sentence structure")
         options = [option for option, checked in {"tone": tone, "voice": voice, "vocabulary": vocabulary, "sentence structure": sentence_structure}.items() if checked]
         if input_type == 'Text':
             return st.text_area("Enter the Example text:"), input_type, temperature, options
-        elif input_type == "File":
-            uploaded_file = st.file_uploader("Upload a file:")
-            if uploaded_file:
-                text = uploaded_file.read().decode('utf-8')
-                return text, input_type, temperature, options
-            else:
-                return None, input_type, temperature, options
+        # elif input_type == "File":
+        #     uploaded_file = st.file_uploader("Upload a file:")
+        #     if uploaded_file:
+        #         text = uploaded_file.read().decode('utf-8')
+        #         return text, input_type, temperature, options
+        #     else:
+        #         return None, input_type, temperature, options
         elif input_type == "Link":
             link = st.text_input("Enter the link:")
             if link:
@@ -78,6 +81,7 @@ if check_password():
     def send_to_openai_api(input_data, input_type, prompt, temperature, options):
         # Process input_data and send it to OpenAI API using the prompt
         options_string = ", ".join(options)
+        print(options_string)
         custom_instruction = f"] in the style of the provided example, capturing its {options_string}. Example: {input_data}"
         full_prompt = f"{prompt}\n {custom_instruction}\n"
 
@@ -108,11 +112,20 @@ if check_password():
             st.write(ai_response)
             previous_outputs.append(ai_response)
             st.session_state.previous_outputs = previous_outputs
+            with open('previous_outputs.json', 'w') as f:
+                json.dump(previous_outputs, f)
 
         st.sidebar.title("Previous Outputs")
         for i, output in enumerate(previous_outputs):
             with st.sidebar.expander(f"Output {i + 1}"):
                 st.write(output)
+    def load_previous_outputs():
+        if os.path.exists('previous_outputs.json'):
+            with open('previous_outputs.json', 'r') as f:
+                st.session_state.previous_outputs = json.load(f)
+        else:
+            st.session_state.previous_outputs = []
 
     if __name__ == "__main__":
+        load_previous_outputs()
         main()
